bfc4928b4343b5b60df37f2089ccf42b27374253
RubasznyChomik <poreba.mateusz@gmail.com>
2023-06-11 21:34:17 +0200

    repaired bug

--- src/pages/components/jokes/jokesList.js
+++ src/pages/components/jokes/jokesList.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import styles from '../../../styles/jokesList.module.css'
 
 import JokeCard from '../jokes/jokeCard'
 
-
-
 export default function JokesList(props) {
+const [showOverflowY, setShowOverflowY] = useState(false);
+
+useEffect(() => {
+  if(props.jokes.length >= 5){
+    
+    setShowOverflowY(true);
+    console.log('overflow true:    '+ showOverflowY);
+  }else{
+    setShowOverflowY(false);
+    console.log('overflow false:    '+ showOverflowY);
+  }
+
+  return () => {
+    
+  }
+}, [props.jokes.length > 5])
+
+
+
   return (
-    <div className={styles.jokes}>
+    <div className={`${styles.jokes} ${showOverflowY ? styles.overflowY : ''}`}>
       {props.jokes.map(j => (
         <JokeCard
           key={j.key}
