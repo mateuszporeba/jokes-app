441b77823c4963a6a2f16bdfc0532c7f17b9fb8b
RubasznyChomik <poreba.mateusz@gmail.com>
2023-06-12 12:20:24 +0200

    x

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from '../../styles/Header.module.css'
 
 import Image from 'next/image';
-import pelikanImage from '@/images/pelikan.png';
+//import pelikanImage from '@/images/pelikan.png';
 // import pimg from '../../../public/pelikan.png'
 export default function Header() {
 
@@ -12,13 +12,7 @@ export default function Header() {
       <div className={styles['h1-container']}>
         <h1 className={styles.h1}>Joke Machine 2k23</h1>
       </div>
-      <Image
-      className={styles.image}
-        src="/pelikan.png"
-        width={500}
-        height={500}
-        alt="Picture of the author"
-      />
+
     </div>
   )
 }
