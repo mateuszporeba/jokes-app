cd93b9f8c11ab48d3218fff46b27226cabfb5f15
RubasznyChomik <poreba.mateusz@gmail.com>
2023-06-12 20:06:19 +0200

    repaired bug

--- src/pages/components/jokes/jokesList.js
+++ src/pages/components/jokes/jokesList.js
@@ -1,18 +1,21 @@
 import React, {useState, useEffect} from 'react'
+import { useMediaQuery } from 'usehooks-ts';
 import styles from '../../../styles/jokesList.module.css'
 
 import JokeCard from '../jokes/jokeCard'
 
 export default function JokesList(props) {
 const [showOverflowY, setShowOverflowY] = useState(false);
+const matches = useMediaQuery('(max-width: 635px)');
+console.log(matches);
 
 useEffect(() => {
-  if(props.jokes.length >= 5){
+  if(props.jokes.length >= matches?3:5){
     setShowOverflowY(true);
   }else{
     setShowOverflowY(false);
   }
-}, [props.jokes.length > 5])
+}, [props.jokes.length >= matches?3:5])
 
 
 
