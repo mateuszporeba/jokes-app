0cded64dbbb1e7fae7b20e0f2e2d192e4d5db950
RubasznyChomik <poreba.mateusz@gmail.com>
2023-06-12 12:54:10 +0200

    repaired picture

--- src/pages/components/header.js
+++ src/pages/components/header.js
@@ -3,18 +3,18 @@ import styles from '../../styles/header.module.css'
 
 import Image from 'next/image';
 //import pelikanImage from '@/images/pelikan.png';
-// import pimg from '../../../public/pelikan.png'
+import Pelikan from '@/images/pelikan.png'
 export default function Header() {
 
   return (
     <div className={styles.headerContainer}>
-      
+
       <div className={styles['h1-container']}>
         <h1 className={styles.h1}>Joke Machine 2k23</h1>
       </div>
       <Image
-      className={styles.image}
-        src="/pelikan.png"
+        className={styles.image}
+        src={Pelikan}
         width={500}
         height={500}
         alt="Picture of the author"
